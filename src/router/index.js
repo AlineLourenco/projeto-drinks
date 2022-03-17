9171133a73fb43846e300da31e04993920bb2bc8
Jainy <jainyestefany@gmail.com>
2022-03-17 10:12:40 -0300

    Esqueleto

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,27 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import MeuInicio from '../views/MeuInicio.vue'
+import ListaDrinks from '../views/ListaDrinks.vue'
+import MenorIdade from '../views/MenorIdade.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'MeuInicio',
+    component: MeuInicio
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    path: '/listadrinks',
+    name: 'ListaDrinks',
+    component: ListaDrinks
+  },
+  {
+    path: '/menoridade',
+    name: 'MenorIdade',
+    component: MenorIdade
+  },
 ]
 
 const router = new VueRouter({
