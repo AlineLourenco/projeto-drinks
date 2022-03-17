9458f7fe4d7745acd2c816084d347508242a387e
AlineLourenco <alinelaurentius@gmail.com>
2022-03-17 10:59:40 -0300

    alterando a página início

--- src/views/MeuInicio.vue
+++ src/views/MeuInicio.vue
@@ -1,6 +1,9 @@
 <template>
     <v-container>
         <h1>Início</h1>
+        <v-img class="inicio-imagem mb-4" src="https://images.pexels.com/photos/605408/pexels-photo-605408.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940">
+        </v-img>
+        <p>Aplicativo voltado para a venda de bebidas alcoólicas a maiores de 18 anos com uma ampla variedades de drinks e bebidas não acoólicas a todos.</p>
     </v-container>
 </template>
 
@@ -11,5 +14,10 @@ export default {
 </script>
 
 <style scoped>
-
+    .inicio-imagem {
+        border-radius: 5px;
+    }
+    h1 {
+        text-align: center;
+    }
 </style>
