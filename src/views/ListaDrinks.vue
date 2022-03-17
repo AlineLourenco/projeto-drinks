28ba04488d88196b369d29314a208b685bf76773
Maria Fernanda <ferdinandamoge@gmail.com>
2022-03-17 12:49:25 -0300

    Consultando API

--- src/views/ListaDrinks.vue
+++ src/views/ListaDrinks.vue
@@ -1,23 +1,41 @@
 <template>
   <v-app>
-    <AppTopBarMaiorIdade/>
-      <v-main>
-        <router-view/>
-      </v-main>
+    <AppTopBarMaiorIdade />
+    <v-main>
+      <router-view />
+      <h2>Drinks Maior Idade</h2>
+      <ul>
+        <li v-for="item of listaMaiores" :key="item.idDrink">
+          <v-img :src="item.strDrinkThumb">{{ item.strDrink }}</v-img>
+        </li>
+      </ul>
+    </v-main>
   </v-app>
 </template>
 
 <script>
-import AppTopBarMaiorIdade from '../components/shared/AppTopBarMaiorIdade.vue'
+import AppTopBarMaiorIdade from "../components/shared/AppTopBarMaiorIdade.vue";
 
 export default {
-  name: 'AppMaiorIdade',
+  name: "AppMaiorIdade",
   components: {
     AppTopBarMaiorIdade,
+  },
+  data() {
+    return {
+      listaMaiores: [],
+    };
+  },
+  created() {
+    fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s")
+      .then((resposta) => resposta.json())
+      .then((json) => {
+        this.listaMaiores = json;
+        console.log(this.listaMaiores);
+      });
   },
 };
 </script>
 
 <style scoped>
-
 </style>
